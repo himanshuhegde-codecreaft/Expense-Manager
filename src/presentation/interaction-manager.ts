2e070c6bcf8e24e0cc68e5eafc78babd8d603214
Himanshu Hegde <himanshu.hegde@codecraft.co.in>
2026-03-31 11:22:24 +0000

    added optional to choose

--- src/presentation/interaction-manager.ts
+++ src/presentation/interaction-manager.ts
@@ -12,13 +12,10 @@ export interface Choice {
   value: string;
 }
 
-// const genders: Choice[] = [{ label: "Male", value: "M" }];
-
-// const expenseTypes: Choice[] = [{ label: "Food", value: "FOOD" }];
 
 export const initialiseInteractionManager = () => {
   const rl = readline.createInterface({ input, output });
-  const ask = async (question: string, options: AskOptions) => {
+  const ask:(question: string, options: AskOptions)=>Promise<string|undefined> = async (question: string, options: AskOptions) => {
     const { defaultAnswer, validator } = options || {};
     return new Promise((resolve) => {
       rl.question(
@@ -33,15 +30,29 @@ export const initialiseInteractionManager = () => {
       );
     });
   };
-  const choose = async (question: string, choices: Choice[]) => {
+  const choose:(question: string, choices: Choice[],optional?:boolean)=>Promise<Choice|undefined>=async (question: string, choices: Choice[],optional)=> {
     console.log(question);
     choices.forEach((choice) => {
       console.log(`${choice.value}. ${choice.label}`);
     });
-    return ask("Please your choice", {
-      validator: (input) => choices.some((choice) => choice.value === input),
+    const choice = await ask("Please your choice", {
+      validator: (input) =>{
+        if(optional && input.trim()=== ''){
+          return true;
+        }
+        return choices.some((choice) => choice.value === input)},
     });
+    return choices!.find(c=>c.value===choice)
   };
+
+  const close = ()=>{
+    rl.close();
+  }
+  return {
+    ask,
+    choose,
+    close
+  }
 };
 
 // const run = async () => {
